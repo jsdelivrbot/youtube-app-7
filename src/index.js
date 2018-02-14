0c592642eadd771211d294d3eab6400d9bb42ca6
Amitay Soffer <amitaysoffer@gmail.com>
2018-02-14 14:45:06 +0200

    try

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
+
 
 const API_KEY = 'AIzaSyBYf1d1OI9RrbBZ8ox-HppCUqyndH8herc';
 
@@ -10,7 +13,7 @@ class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = { videos: [] }
-        YTSearch({ key: API_KEY, term: 'Messi' }, videos => {
+        YTSearch({ key: API_KEY, term: 'surfboards' }, videos => {
             this.setState({ videos }); // videos: videos
         });
     };
@@ -18,6 +21,8 @@ class App extends React.Component {
         return (
             <div>
                 <SearchBar />
+                <VideoDetail video={this.state.videos[0]} />
+                <VideoList videos={this.state.videos} />
             </div>
         );
     };
